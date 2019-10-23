9abe8d9f45e31e6cf60a6a182497dee2d31807f9
softHY <hyseo33@gmail.com>
2019-10-23 17:37:11 +0900

    191023

--- 03_Django/06_django_form/accounts/models.py
+++ 03_Django/06_django_form/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+######### accounts
+
+class User(AbstractUser):
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="followings")
